7e86ab4e6b35340e20baacb6ff5da28cf50bbe03
Dawid <wadowicowin@gmail.com>
2026-05-06 21:58:50 +0200

    Dodano kolejne assety. Poszerzono mape.

---
+++ assets/maps/decor.tsx
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="decor" tilewidth="16" tileheight="16" tilecount="84" columns="7">
+ <image source="decor_tileset.png" width="112" height="192"/>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.86957" y="11.913" width="2.26087" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.82609" y="11.8261" width="2.3913" height="2.26087"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="12.3478">
+    <polygon points="0,0 6.95652,1.04348 11.913,-0.913043 9.95652,-4.3913 2.04348,-4.47826"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.56522" y="8.52174">
+    <polygon points="0,0 5.82609,2.56522 10.1304,0.0434783 10,-1.56522 1.43478,-1.56522"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.95652" y="8.43478" width="12.1304" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.84784" y="7.52174" width="9.7391" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.84784" y="7.95652" width="9.7391" height="5.47826"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.02176" y="8.52174" width="10.8695" height="5.13043"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.89132" y="8.47826" width="12.1304" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.91306" y="7.3913" width="9.7391" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.91306" y="8.04348" width="9.7391" height="5.47826"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.00003" y="8.34783" width="10.8695" height="5.13043"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.95652" y="10.0435" width="5" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.06522" y="10.0652" width="5" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="65">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.06522" y="10.1087" width="5" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.06522" y="10.1087" width="5" height="2.86957"/>
+  </objectgroup>
+ </tile>
+ <tile id="72">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.8913" y="11.1087" width="5" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="73">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.97826" y="11.0652" width="5" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="79">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.97826" y="11.0217" width="5" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.93478" y="11.0652" width="5" height="3"/>
+  </objectgroup>
+ </tile>
+</tileset>
