cf995067f4523fa7303e0209ede584d8252fa8b2
Dawid <wadowicowin@gmail.com>
2026-05-20 23:10:40 +0200

    Poprawki

--- utils/src/main/java/com/polsl/poiw/TexturePackerTool.java
+++ utils/src/main/java/com/polsl/poiw/TexturePackerTool.java
@@ -5,9 +5,9 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 public class TexturePackerTool {
 
     public static void main(String[] args) {
-        String inputDir = "assets/raw/items";
+        String inputDir = "assets/raw/creatures";
         String outputDir = "assets/graphics";
-        String packFileName = "items";
+        String packFileName = "creatures";
 
         TexturePacker.process(inputDir, outputDir, packFileName);
     }
